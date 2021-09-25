ebca02ec06966aabc167b9cd323c43a92ecc85df
denisdimarco <denisdimarcoleda@gmail.com>
2021-09-24 21:39:56 -0300

    punto 3

--- MySQL-E2/Ejercicio2.sql
+++ MySQL-E2/Ejercicio2.sql
@@ -1,1 +1,15 @@
-SELECT 
+
+# USE personal;
+
+#1. Obtener los datos completos de los empleados.
+SELECT * FROM empleados; 
+#2. Obtener los datos completos de los departamentos.
+SELECT * FROM departamentos;
+#3. Listar el nombre de los departamentos.
+SELECT nombre_depto AS 'Nombre departamentos' from departamentos;
+#4. Obtener el nombre y salario de todos los empleados.
+SELECT nombre AS 'Nombre Empleados', sal_emp AS 'Salario empleados' FROM empleados;
+#5. Listar todas las comisiones.
+#6. Obtener los datos de los empleados cuyo cargo sea ‘Secretaria’.
+#7. Obtener los datos de los empleados vendedores, ordenados por nombre
+#alfabéticamente.

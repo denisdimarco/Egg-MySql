fd6d0fce0cfa668bff3051eae5ca0c3fd1c872ce
denisdimarco <denisdimarcoleda@gmail.com>
2021-09-27 01:10:51 -0300

    Update Ejercicio2.sql

--- MySQL-E2/Ejercicio2.sql
+++ MySQL-E2/Ejercicio2.sql
@@ -3,22 +3,30 @@ USE personal;
 
 #1. Obtener los datos completos de los empleados.
 SELECT * FROM empleados; 
+
 #2. Obtener los datos completos de los departamentos.
 SELECT * FROM departamentos;
+
 #3. Listar el nombre de los departamentos.
 SELECT nombre_depto AS 'Nombre departamentos' from departamentos;
+
 #4. Obtener el nombre y salario de todos los empleados.
 SELECT nombre AS 'Nombre Empleados', sal_emp AS 'Salario empleados' FROM empleados;
+
 #5. Listar todas las comisiones.
 SELECT comision_emp AS 'Comisiones' FROM empleados;
+
  #6. Obtener los datos de los empleados cuyo cargo sea ‘Secretaria’.
 SELECT * FROM empleados WHERE cargo_emp = 'Secretaria';
+
 #7. Obtener los datos de los empleados vendedores, ordenados por nombre alfabéticamente.
 SELECT * FROM empleados WHERE cargo_emp = 'Vendedor' ORDER BY nombre ASC;
 
 #8. Obtener el nombre y cargo de todos los empleados, ordenados por salario de menor a mayor.
+SELECT nombre, cargo_emp FROM empleados ORDER BY sal_emp ASC;
 
 #9. Elabore un listado donde para cada fila, figure el alias ‘Nombre’ y ‘Cargo’ para las respectivas tablas de empleados.
+
 #10. Listar los salarios y comisiones de los empleados del departamento 2000, ordenado por comisión de menor a mayor.
 #11. Obtener el valor total a pagar que resulta de sumar el salario y la comisión de los empleados del departamento 3000 una bonificación de 500, en orden alfabético
 #del empleado.
